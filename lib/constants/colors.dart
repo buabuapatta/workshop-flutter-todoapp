fd4c3c0171e67170f2b5793c381dce4a227bf168
DESKTOP-43AVUG3\BAIBUA <Patharapond@gmail.com>
2023-08-26 13:02:12 +0700

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFFF0000);
+const Color tdBlue = Color(0xFF006EFF);
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGray = Color(0xFF777777);
+
+const Color tdBGColor = Color(0xFFEEF6FF);
